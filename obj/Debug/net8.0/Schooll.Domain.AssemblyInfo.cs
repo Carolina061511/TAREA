91a5584bba7d34f7aad92078c0281828a6a99b8a
Carolina061511 <guerrerocarolina069@gmail.com>
2024-11-22 22:20:39 -0400

    Solución de conflictos y adición de archivos faltantes

--- obj/Debug/net8.0/Schooll.Domain.AssemblyInfo.cs
+++ obj/Debug/net8.0/Schooll.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Schooll.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a3625c07f846645609f3accf9eb19907699692")]
 [assembly: System.Reflection.AssemblyProductAttribute("Schooll.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Schooll.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
